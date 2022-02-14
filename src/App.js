e8c4b6887c17bbb403f6b6ccf6606f86b6b1f9fc
PUSHKAR <96309896+dakshpushkar@users.noreply.github.com>
2022-02-14 23:40:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Resturant from "./Components/Reaturant";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Resturant />
+    </div>
+  );
+}
